a2a446f2a7d74eef17e73a7222e1e980bc8d62ea
wangding <wangding#123>
2019-02-26 22:17:54 +0800

    线性表数据结构是基本实现

---
+++ datasturcture/src/liner_table/cn/fenqing168/study/code/test/Test.java
@@ -0,0 +1,14 @@
+package liner_table.cn.fenqing168.study.code.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        String a = new String("1");
+        String b = new String("2");
+        String c = a + b;
+        System.out.println(System.identityHashCode(1));
+
+    }
+
+}
