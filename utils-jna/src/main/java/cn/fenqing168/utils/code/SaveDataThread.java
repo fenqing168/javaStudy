a3f919ed337d87ef0d5f8836bc145493ed8e6d9c
fenqing <1286976336@qq.com>
2019-04-11 19:33:13 +0800

    。。。。

--- utils-jna/src/main/java/cn/fenqing168/utils/code/SaveDataThread.java
+++ utils-jna/src/main/java/cn/fenqing168/utils/code/SaveDataThread.java
@@ -1,18 +1,30 @@
 package cn.fenqing168.utils.code;
 
-import cn.fenqing168.utils.bean.ConfigBean;
-
 public class SaveDataThread implements Runnable {
 
+    /**
+     * 要保存的数据
+     */
     private double[] datas;
 
+    /**
+     * 保存的逻辑代码模板
+     */
     private SaveDataTemplate saveDataTemplate;
 
+    /**
+     * 创建该线程必须提供指定的对象
+     * @param datas
+     * @param saveDataTemplate
+     */
     public SaveDataThread(double[] datas, SaveDataTemplate saveDataTemplate) {
         this.datas = datas;
         this.saveDataTemplate = saveDataTemplate;
     }
 
+    /**
+     * 开始存储
+     */
     @Override
     public void run() {
         saveDataTemplate.saveData(datas);
